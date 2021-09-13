5814cf398a53870aaf2cc18c07b353098da5ede4
sofo <soffoalbert@gmail.com>
2021-09-13 19:46:20 +0200

    added status constants to the handler

--- src/components/pdf_handler/pdf.component.ts
+++ src/components/pdf_handler/pdf.component.ts
@@ -4,6 +4,8 @@ import { TYPES } from '../../config/container/types';
 import { IPDFHandler } from './pdf.handler';
 import { PDFRecord } from './pdf.record.model';
 import { Bootstrap } from '../../config/container/Bootstrap';
+import { DuplicateException } from './duplicate.exception';
+import statusCode from 'http-status-codes';
 
 /**
  * @export
@@ -21,9 +23,12 @@ export async function store(req: Request, res: Response, next: NextFunction): Pr
         console.log(`received request to process URL: ${req.body}`);
         const pdfRecord: PDFRecord = await pdfHandler.handle(req.body);
 
-        res.status(201).json(pdfRecord);
+        res.status(statusCode.CREATED).json(pdfRecord);
     } catch (error) {
-        next(error.message);
+        if (error instanceof DuplicateException) {
+            res.status(statusCode.BAD_REQUEST).json({ message: error.message, statusCode: statusCode.BAD_REQUEST });
+        }
+        res.status(statusCode.INTERNAL_SERVER_ERROR).json({ message: error.message, statusCode: statusCode.INTERNAL_SERVER_ERROR });
     }
 }
 
@@ -41,8 +46,8 @@ export async function findAll(req: Request, res: Response, next: NextFunction): 
     try {
         const pdfRecords: PDFRecord[] = await pdfHandler.list();
 
-        res.status(200).json(pdfRecords);
+        res.status(statusCode.OK).json(pdfRecords);
     } catch (error) {
-        next(error.message);
+        res.status(statusCode.INTERNAL_SERVER_ERROR).json({ message: error.message, statusCode: statusCode.INTERNAL_SERVER_ERROR });
     }
 }
